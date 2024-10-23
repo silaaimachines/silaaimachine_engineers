c1aa8920da378feb0404b55b84a41e5555859548
Marshal Tudu <iammarshal22@gmail.com>
2024-10-23 15:04:10 +0530

    Marshal

--- nextjs_smengg/app/_components/DesktopHeader.js
+++ nextjs_smengg/app/_components/DesktopHeader.js
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import { Button } from '@/components/ui/button';
 import { Search, PhoneCall } from 'lucide-react';
 import siteIcon from '../public/images/02.png';
+import Link from 'next/link';
 
 
 const DesktopHeader = () => {
@@ -14,6 +15,9 @@ const DesktopHeader = () => {
       </div>
 
       <div className='flex gap-5 items-center' style={{ width: '35%' }}>
+        <Link href="/">Home</Link>
+        <Link href="/Store">Store</Link>
+
         <div className='flex gap-3 items-center border rounded-full p-2 justify-between w-full'>
           <input type='text' placeholder='Search' className='outline-none px-3 w-full' />
           <Search className='h-5 w-5' />
