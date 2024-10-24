b18983cac83b98056b2083f52cf86dc0b636514c
Marshal Tudu <iammarshal22@gmail.com>
2024-10-24 10:28:12 +0530

    Marshal

--- nextjs_smengg/app/_components/Slider.js
+++ nextjs_smengg/app/_components/Slider.js
@@ -5,7 +5,7 @@ import {
     CarouselItem,
     CarouselNext,
     CarouselPrevious,
-  } from "@/components/ui/carousel"
+  } from "../../components/ui/carousel"
 
 import Autoplay from "embla-carousel-autoplay"
 import Image from 'next/image'
