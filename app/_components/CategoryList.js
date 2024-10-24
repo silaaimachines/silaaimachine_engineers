acb7930a046ee91db765d3068c64e72c4193296e
Marshal Tudu <iammarshal22@gmail.com>
2024-10-24 17:57:56 +0530

    w

--- app/_components/CategoryList.js
+++ app/_components/CategoryList.js
@@ -12,9 +12,9 @@ const CategoryList = ({ categoryList }) => {
 
   return (
     <div className='px-5'>
-      <div className='flex items-center gap-2'>
-      <h1 className='py-5 text-2xl font-semibold'>Shop by</h1>
-      <h1 className='text-3xl font-semibold underline decoration-red-500'>Categories</h1>
+      <div className='flex items-center gap-1'>
+      <h1 className='py-5 font-semibold text-md md:text-2xl'>Shop by</h1>
+      <h1 className='font-semibold underline decoration-red-500 text-lg md:text-3xl'>Categories</h1>
       </div>
       <ScrollArea className="w-full overflow-hidden">
         <div className="flex space-x-5">
@@ -25,17 +25,17 @@ const CategoryList = ({ categoryList }) => {
               animate="visible"
               variants={itemVariants}
               transition={{ duration: 2, delay: index * 0.1 }} // Delay for staggered effect
-              className='flex flex-col items-center justify-center transition duration-300 ease-in-out rounded-lg border shrink-0 shadow-sm cursor-pointer hover:border-[#e42584]'
+              className='flex flex-col items-center justify-center transition duration-300 ease-in-out rounded-t-lg border shrink-0 shadow-sm cursor-pointer hover:border-[#e42584]'
             >
               <Image
                 src={process.env.NEXT_PUBLIC_BACKEND_BASE_URL + category?.Images?.url}
                 width={200} // Adjust size as necessary
                 height={200} // Adjust size as necessary
                 alt='Slider Image'
-                className='h-[200px] w-[200px] object-contain rounded-t-lg'
+                className='object-contain rounded-t-lg h-auto w-full/2'
               />
-              <div className='w-full bg-[#e42584] rounded-b-lg'>
-                <h2 className='m-2 text-sm text-center text-white'>{category?.Name}</h2>
+              <div className='w-full bg-[#e42584]'>
+                <h2 className='m-2 text-center text-white text-sm md:text-'>{category?.Name}</h2>
               </div>
             </motion.div>
           ))}
