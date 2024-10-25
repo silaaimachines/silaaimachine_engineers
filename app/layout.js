1fec8f11b2ee039120198610a4b9060a0f31dd7d
Marshal Tudu <iammarshal22@gmail.com>
2024-10-25 15:39:18 +0530

    k

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 import Header from "./_components/Header";
 import Footer from "./_components/Footer";
 import { ThemeProvider } from "@/components/theme-provider";
-import Test from "./_components/Test";
+import { FaWhatsapp, FaPhoneAlt, FaEnvelope } from 'react-icons/fa';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -33,8 +33,34 @@ export default function RootLayout({ children }) {
           enableSystem
           disableTransitionOnChange
         >
+          {/* Floating Contact Icons */}
+          <div className="fixed right-5 bottom-10 space-y-4 hidden md:flex lg:flex flex-col z-50">
+            <a
+              href="https://wa.me/+919437782677"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="p-3 bg-green-500 text-white rounded-full"
+              aria-label="Contact us on WhatsApp"
+            >
+              <FaWhatsapp size={24} />
+            </a>
+            <a
+              href="tel:+919437782677"
+              className="p-3 bg-blue-500 text-white rounded-full"
+              aria-label="Call us"
+            >
+              <FaPhoneAlt size={24} />
+            </a>
+            <a
+              href="mailto:satya.silaaimachines@gmail.com"
+              className="p-3 bg-red-500 text-white rounded-full"
+              aria-label="Email us"
+            >
+              <FaEnvelope size={24} />
+            </a>
+          </div>
           <Header />
-          {children}         
+          {children}
           <Footer />
         </ThemeProvider>
       </body>
