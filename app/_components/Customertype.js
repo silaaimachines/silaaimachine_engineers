9653fbb007bc097fcd725d26c732e04044fc0bc2
Gaurav <gs0023706@gmail.com>
2024-10-26 19:16:34 +0530

    fdhdf

--- app/_components/Customertype.js
+++ app/_components/Customertype.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Link from 'next/link'; // Import Link from next/link
+import Link from 'next/link';
 import { motion, useInView } from 'framer-motion';
 import Image from 'next/image';
 import { ScrollArea, ScrollBar } from "../../components/ui/scroll-area";
@@ -7,49 +7,51 @@ import { ScrollArea, ScrollBar } from "../../components/ui/scroll-area";
 const Customertype = ({ customerTypeList }) => {
   // Animation variants
   const itemVariants = {
-    hidden: { opacity: 0, scale: 0.9 },
-    visible: { opacity: 1, scale: 1 },
+    hidden: { opacity: 0, scale: 0.9, filter: 'blur(8px)' },
+    visible: { opacity: 1, scale: 1, filter: 'blur(0px)' },
   };
 
-  // Use the useInView hook to check visibility
   const ref = React.useRef(null);
   const isInView = useInView(ref, { triggerOnce: true, threshold: 0.2 });
 
   return (
     <div ref={ref}>
-      <div className='px-5'>
-        <div className='flex items-center gap-2'>
-          <h1 className='py-5 text-2xl font-semibold'>Shop by</h1>
-          <h1 className='text-3xl font-semibold underline decoration-red-500'>Customer-Type</h1>
-        </div>
-        <ScrollArea className="w-full overflow-hidden">
-          <div className="flex space-x-5">
-            {customerTypeList.map((customer_type, index) => (
-              <Link key={index} href={`/customer-type/${customer_type.slug}`} passHref>
-                <motion.div
-                  initial="hidden"
-                  animate={isInView ? "visible" : "hidden"}
-                  variants={itemVariants}
-                  transition={{ duration: 1, delay: index * 0.1 }}
-                  className='flex flex-col items-center justify-center transition duration-300 ease-in-out rounded-lg border shrink-0 shadow-sm cursor-pointer hover:border-[#e42584]'
-                >
-                  <Image
-                    src={process.env.NEXT_PUBLIC_BACKEND_BASE_URL + customer_type?.Image?.url}
-                    width={200}
-                    height={200}
-                    alt='Slider Image'
-                    className='h-[200px] w-[200px] object-contain rounded-t-lg'
-                  />
-                  <div className='w-full bg-black dark:bg-gray-700 rounded-b-lg'>
-                    <h2 className='m-2 text-sm text-center text-white'>{customer_type?.Name}</h2>
-                  </div>
-                </motion.div>
-              </Link>
-            ))}
-          </div>
-          <ScrollBar orientation="horizontal" />
-        </ScrollArea>
+      <div className='flex items-center gap-1 md:gap-2 py-2 md:py-5 px-5'>
+        <h1 className='font-semibold text-lg md:text-2xl'>Shop by</h1>
+        <h1 className='font-semibold underline decoration-red-500 text-lg md:text-2xl'>Customer-Type</h1>
       </div>
+      
+  
+        <div className="grid grid-cols-3 lg:grid-cols-6 gap-1">
+          {customerTypeList.slice(0, 6).map((customer_type, index) => (
+            <Link key={index} href={`/customer-type/${customer_type.slug}`} passHref>
+              <motion.div
+                initial="hidden"
+                animate={isInView ? "visible" : "hidden"}
+                variants={itemVariants}
+                transition={{ duration: 0.5, delay: index * 0.1 }}
+                className='flex flex-col items-center justify-center transition duration-300 ease-in-out rounded-2xl border shrink-0 cursor-pointer hover:border-[#e42584] w-full h-auto shadow-sm'
+              >
+                <div className="relative">
+                  {customer_type?.Image?.url && (
+                    <Image
+                      src={process.env.NEXT_PUBLIC_BACKEND_BASE_URL + customer_type.Image.url}
+                      width={200}
+                      height={200}
+                      alt={customer_type?.Image?.alternativeText || 'Customer Type Image'}
+                      className='object-cover rounded-t-2xl w-full h-auto'
+                    />
+                  )}
+                </div>
+                <div className='w-full bg-black dark:bg-gray-700 rounded-b-2xl'>
+                  <h2 className='text-center text-xs md:text-sm py-2 text-white'>{customer_type?.Name}</h2>
+                </div>
+              </motion.div>
+            </Link>
+          ))}
+        </div>
+        
+      
     </div>
   );
 };
