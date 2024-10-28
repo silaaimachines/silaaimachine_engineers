7473264f803c74b800ab82e72ec7ba8dae44a097
Marshal Tudu <iammarshal22@gmail.com>
2024-10-28 09:46:12 +0530

    eee

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: false,
     images: {
-        domains: ['silaaimachines.com', 'localhost', '192.168.1.10']
+        domains: ['silaaimachines.com', 'localhost', '192.168.29.181']
     },
     /* output: 'export' */
 };
