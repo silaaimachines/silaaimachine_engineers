799b4344a1f6a899c7ecd6fcee3b5d7fb6010556
Gaurav <gs0023706@gmail.com>
2024-11-01 17:56:40 +0530

    kk

--- app/category/[slug]/page.js
+++ app/category/[slug]/page.js
@@ -6,6 +6,10 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { BackgroundGradient } from '@/components/ui/background-gradient';
 import BoxReveal from '@/components/ui/box-reveal';
+import BlurIn from '@/components/ui/blur-in';
+import FlipText from '@/components/ui/flip-text';
+
+
 
 export default function CategoryPage() {
   const params = useParams();
@@ -64,8 +68,14 @@ export default function CategoryPage() {
 
   return (
     <div className="p-3 md:p-5">
-      <h2 className="text-xl font-semibold mb-4">{category.Name}</h2>
+    
+<FlipText
+  word={category.Name || ''} // Provide a fallback to an empty string if category.Name is undefined
+  className="text-4xl font-bold -tracking-widest text-black dark:text-white md:text-7xl md:leading-[5rem]"
+/>
 
+
+      
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 items-center gap-3 md:gap-4 lg:gap-6 py-2 md:py-5">
         {products.map((product, index) => {
           const { BasePrice, DiscountPrice, Name, Images, slug } = product;
