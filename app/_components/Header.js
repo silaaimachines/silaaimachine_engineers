9c75b7762809f9c40e67a406eb47b0522a421a69
Gaurav <gs0023706@gmail.com>
2024-11-01 16:22:30 +0530

    hh

--- app/_components/Header.js
+++ app/_components/Header.js
@@ -97,7 +97,7 @@ const Header = () => {
                         <NavigationMenuLink asChild>
                           <Link
                             href={`/category/${category.slug}`}
-                            className="block p-3 rounded-md hover:bg-theme_color hover:text-white"
+                            className="block p-3 rounded-md hover:bg-theme_color hover:text-white text-sm"
                           >
                             {category.Name}
                           </Link>
