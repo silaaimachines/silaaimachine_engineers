50ce51df8e09c59ddab5d20ac8b101ce3e77195d
Marshal Tudu <iammarshal22@gmail.com>
2024-11-14 17:39:32 +0530

    000

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const nextConfig = {
   images: {
     domains: [
       "silaaimachines.com",
+      "admin.silaaimachines.com",
       process.env.NEXT_PUBLIC_BACKEND_BASE_URL,
       "192.168.29.181",
       "192.168.29.15",
