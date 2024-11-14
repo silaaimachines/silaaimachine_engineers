5396f99f25ce1cbc0c4b56283d02358fe29a2768
Gaurav <gs0023706@gmail.com>
2024-11-14 13:30:09 +0530

    DD

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,11 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ["silaaimachines.com", process.env.NEXT_PUBLIC_BACKEND_BASE_URL],
+    domains: [
+      "silaaimachines.com",
+      process.env.NEXT_PUBLIC_BACKEND_BASE_URL,
+      "192.168.29.181",
+    ],
   },
   /* output: "export", */
 };
