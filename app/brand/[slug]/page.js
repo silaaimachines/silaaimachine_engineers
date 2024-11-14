ac016938fb81a32b04d6dd60fd0e5a30340e2184
Marshal Tudu <iammarshal22@gmail.com>
2024-11-14 16:28:10 +0530

    rrr

--- app/brand/[slug]/page.js
+++ app/brand/[slug]/page.js
@@ -1,10 +1,20 @@
 import BrandPageContent from "./BrandPageContent";
 
 export async function generateMetadata({ params, searchParams }, parent) {
-  const { slug } = await params; // Await the params object here
-  console.log(slug);
+  const { slug } = await params;
+
+  // Capitalize the first letter of each word
+  const title = slug
+    ? slug
+        .split("-")
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(" ")
+    : "";
+
+  console.log(title);
+
   return {
-    title: slug ? slug : "",
+    title: `${title} Sewing Machine & Accessories`,
   };
 }
 
