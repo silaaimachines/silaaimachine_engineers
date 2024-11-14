820c1887027ed66576e1603b97d2bc5e7616d45b
Marshal Tudu <iammarshal22@gmail.com>
2024-11-14 18:07:08 +0530

    ii

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,6 @@ const nextConfig = {
     domains: [
       "silaaimachines.com",
       "admin.silaaimachines.com",
-      process.env.NEXT_PUBLIC_BACKEND_BASE_URL,
       "192.168.29.181",
       "192.168.29.15",
     ],
